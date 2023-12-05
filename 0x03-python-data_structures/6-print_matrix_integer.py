f6ecffa917e2b860b48ae1050f7097af3b683988
Amro Madani <33586588+aali1189@users.noreply.github.com>
2023-12-05 03:53:24 +0300

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
     if not matrix:
         print()
     else:
-        # Loop through the rows
+      
         for row in matrix:
-            # Loop through the items in row
+            
             for item in row:
-                # Add space between items if not last item in row
+               
                 if row.index(item) != len(row) - 1:
                     endspace = " "
                 else:
                     endspace = ""
-                # Print the items in the row
+                
                 print("{:d}".format(item), end=endspace)
             print()
