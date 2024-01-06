7cee6700da4d73bdccd7c8628e8ed80dcda0a71e
Amro Madani <33586588+aali1189@users.noreply.github.com>
2024-01-07 00:00:26 +0300

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # 0-add_integer.py
-# Brennan D Baraban <375@holbertonschool.com>
 """Defines an integer addition function."""
-
 
 def add_integer(a, b=98):
     """Return the integer addition of a and b.
